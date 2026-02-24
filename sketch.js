deb037a2f3f3546c952ee6a19ad9ea1847d4aa2e
YulianaFelix <L24170543@culiacan.tecnm.mx>
2026-02-24 15:43:24 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,22 @@
-body{
-    background-color: #4e84b6;
-    font-family: Arial, sans-serif;
-    color: rgb(255, 255, 255);
-    margin: 2;
-    padding: 2;
+let x = 300;       // posición horizontal
+let y = 0;         // posición vertical
+let velocidad = 5; // velocidad 5
+let tamaño = 40;   // tamaño de la pelota
+
+function setup() {
+  createCanvas(600, 400); // tamaño del canvas
+  frameRate(30);          // 30 FPS
 }
-head{
-    background-color: #ab97e4;
-    color: rgb(255, 255, 255);
-    padding: 2;
-}
-h1{
-    font-size: 32px;
-    margin-bottom: 12px;
+
+function draw() {
+  background(220); // limpia la pantalla cada frame
+
+  y += velocidad;  // movimiento vertical
+
+  // rebote arriba y abajo
+  if (y > height - tamaño || y < 0) {
+    velocidad *= -1;
+  }
+
+  ellipse(x, y, tamaño, tamaño); // dibuja la pelota
 }
