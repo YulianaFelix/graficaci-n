53ef1c923fd9d4263283483f9c89a619f374ae4f
YulianaFelix <L24170543@culiacan.tecnm.mx>
2026-02-24 15:02:51 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+body{
+    background-color: #4e84b6;
+    font-family: Arial, sans-serif;
+    color: rgb(255, 255, 255);
+    margin: 2;
+    padding: 2;
+}
+head{
+    background-color: #ab97e4;
+    color: rgb(255, 255, 255);
+    padding: 2;
+}
+h1{
+    font-size: 32px;
+    margin-bottom: 12px;
+}
